10cc9fa7e4397cd7c164df2a8d7c9b3e93ad6505
Natasha <Impedansa@Lenovo-PC>
2017-06-08 15:25:18 +0200

    Izmenjen frontend i view/controller analitike izvoda u izradi.

--- app/controllers/AnalitikeIzvoda.java
+++ app/controllers/AnalitikeIzvoda.java
@@ -3,16 +3,19 @@ package controllers;
 import java.util.List;
 
 import models.AnalitikaIzvoda;
-import models.Drzava;
 import play.mvc.Controller;
 
 public class AnalitikeIzvoda extends Controller {
 	
 	public static void show(String mode){
-		List<AnalitikaIzvoda> analitikeIzvoda = AnalitikaIzvoda.findAll();
-		if (mode == null || mode.equals(""))
-			mode = "edit";
-		render(analitikeIzvoda, mode);
+			List<AnalitikaIzvoda> analitikeIzvoda = AnalitikaIzvoda.findAll();
+			if (mode == null || mode.equals(""))
+				mode = "edit";
+			render(analitikeIzvoda, mode);
+		}
+	
+	public static void filter() {
+		
 	}
 
 }
